4ee3255fc8af6c310eeacb8c5b76b46325e09ad7
Elinaldo Jr <esgjunior@uesc.br>
2022-03-09 21:36:48 -0300

    Corrige erros dos arquivos script.js e style.css

--- script.js
+++ script.js
@@ -14,36 +14,50 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-async function updateCartTotalPrice(totalPrice, op){
+async function updateCartTotalPrice(totalPrice, op) {
   const cartTotalPrice = document.querySelector('.total-price');
 
-  if(cartTotalPrice.textContent.toString() === ""){
+  if (cartTotalPrice.textContent.toString() === '') {
     cartTotalPrice.innerText = totalPrice;
-  }
-  else{
+  } else {
     let newPrice = parseFloat(cartTotalPrice.innerText);
-    if(op === '+')
+    if (op === '+') {
       newPrice += await parseFloat(totalPrice);
-    else
+    } else {
       newPrice -= await parseFloat(totalPrice);
+    }
 
-    cartTotalPrice.innerText = newPrice ;
+    cartTotalPrice.innerText = newPrice;
   }
 
   return cartTotalPrice;
+}
+
+// 3. Remova o item do carrinho de compras ao clicar nele
+function cartItemClickListener(event, sku, salePrice) {
+  event.target.remove();
+
+  localStorage.removeItem(sku);
+
+  updateCartTotalPrice(salePrice, '-');
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', (event) => 
+    cartItemClickListener(event, sku, salePrice));
+  return li;
 }
 
 // 2. Adicione o produto ao carrinho de compras
 async function addItemsCart(sku) {
   const item = await (await fetch(`${URL}/items/${sku}`)).json();
   const { title, price } = item;
-  const itemData = {
-    sku,
-    name: title,
-    salePrice: price,
-  };
+  const itemData = { sku, name: title, salePrice: price };
 
-  if(!localStorage.getItem(itemData.sku)){
+  if (!localStorage.getItem(itemData.sku)) {
     document.querySelector('.cart__items').appendChild(createCartItemElement(itemData));
 
     updateCartTotalPrice(price, '+');
@@ -69,93 +83,64 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-// 3. Remova o item do carrinho de compras ao clicar nele
-function cartItemClickListener(event, sku, salePrice) {
-  event.target.remove();
-
-  localStorage.removeItem(sku);
-
-  updateCartTotalPrice(salePrice, '-');
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', (event) => 
-    cartItemClickListener(event, sku, salePrice));
-  return li;
-}
-
-//5. Some o valor total dos itens do carrinho de compras
-function createCartTotalPrice(){
+// 5. Some o valor total dos itens do carrinho de compras
+function createCartTotalPrice() {
   const labelTotalPrice = document.createElement('span');
   labelTotalPrice.className = 'label-total-price';
-  labelTotalPrice.innerText = 'Subtotal: R$ '
+  labelTotalPrice.innerText = 'Subtotal: R$ ';
 
   const cart = document.querySelector('.cart');
   cart.insertBefore(labelTotalPrice, cart.childNodes[2]);
 
   const cartTotalPrice = document.createElement('span');
-	cartTotalPrice.className = 'total-price';
-
+  cartTotalPrice.className = 'total-price';
+  
   return cartTotalPrice;
 }
 
-//4. Carregue o carrinho de compras através do LocalStorage ao iniciar a página
+// 4. Carregue o carrinho de compras através do LocalStorage ao iniciar a página
 function allItemsStorage() {
-  let totalPrice = 0
+  let totalPrice = 0;
 
-  for(var i=0; i<localStorage.length; i++) {
+  for (let i = 0; i < localStorage.length; i += 1) {
     const key = localStorage.key(i);
     const pull = (JSON.parse(localStorage.getItem(key)));
     
     totalPrice += pull.salePrice;
-    const itemData = {
-      sku: pull.sku,
-      name: pull.name,
-      salePrice: pull.salePrice,
-    };
+    const itemData = { sku: pull.sku, name: pull.name, salePrice: pull.salePrice };
     document.querySelector('.cart__items').appendChild(createCartItemElement(itemData));
   }
 
   updateCartTotalPrice(totalPrice);
 }
 
-//7. Adicione um texto de "loading" durante uma requisição à API
-function createLoading(){
+// 7. Adicione um texto de "loading" durante uma requisição à API
+function createLoading() {
   const loading = document.createElement('span');
   loading.className = 'loading';
   loading.innerText = 'loading...';
 
-  itemsContainer = document.querySelector('.items');
+  const itemsContainer = document.querySelector('.items');
   itemsContainer.appendChild(loading);
 }
 
-//1. Crie uma listagem de produtos
+// 1. Crie uma listagem de produtos
 async function getItems(query) {
   const itemsJson = await (await fetch(`${URL}/sites/MLB/search?q=${query}`)).json();
   const itemsResults = itemsJson.results;
 
   itemsResults.forEach(({ id, title, thumbnail }) => {
-    const itemData = {
-      sku: id,
-      name: title,
-      image: thumbnail,
-    };
+    const itemData = { sku: id, name: title, image: thumbnail };
     document.querySelector('.items')
     .appendChild(createProductItemElement(itemData));
   });
 
-  if(document.querySelector('.loading'))
+  if (document.querySelector('.loading')) {
     document.querySelector('.loading').remove();
+  }
 
   const cart = document.querySelector('.cart');
-  cart.insertBefore(createCartTotalPrice(), cart.childNodes[3] );
+  cart.insertBefore(createCartTotalPrice(), cart.childNodes[3]);
 
   allItemsStorage();
 }
@@ -165,15 +150,15 @@ window.onload = () => {
 
   getItems('computador');
 
-  //6. Crie um botão para limpar carrinho de compras
+  // 6. Crie um botão para limpar carrinho de compras
   const cleanCart = document.querySelector('.empty-cart');
 
   cleanCart.addEventListener('click', () => {
     const cartTotalPrice = document.querySelector('.total-price');
-    cartTotalPrice.innerText = "0.00";
+    cartTotalPrice.innerText = '0.00';
     
     document.querySelectorAll('.cart__item').forEach((item) => item.remove());
     
-    localStorage.clear()
+    localStorage.clear();
   });
 };
